985001c38c6dcb0eabad942a510ec7c8ef0322f7
alex <hortinstein@gmail.com>
2019-03-04 20:41:58 -0500

    kinda figured it out

--- src/main.rs
+++ src/main.rs
@@ -1,269 +1,291 @@
-use rustc_test::*;
-use std::env;
-use std::error;
-use std::ffi::OsStr;
-use std::fs::{self, File};
-use std::io::{self, Write};
-use std::path::{Path, PathBuf};
-use std::process::{self, Command};
-use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
-use std::sync::mpsc::{channel, Sender};
-use std::thread;
-use std::time::Duration;
+// use rustc_test::*;
+// use std::env;
+// use std::error;
+// use std::ffi::OsStr;
+// use std::fs::{self, File};
+// use std::io::{self, Write};
+// use std::path::{Path, PathBuf};
+// use std::process::{self, Command};
+// use std::sync::atomic::{AtomicUsize, Ordering, ATOMIC_USIZE_INIT};
+// use std::sync::mpsc::{channel, Sender};
+// use std::thread;
+// use std::time::Duration;
 
-#[macro_export]
-macro_rules! cmdtest {
-    ($name:ident, $cmd:expr, $fun:expr) => {
-        #[test]
-        fn $name() {
-            let command = Command::new($cmd);
-            let mut cmd = TestCommand {
-                dir: "".to_string(),
-                cmd: command,
-            };
-            $fun(cmd);
-        }
-    };
-}
+// #[macro_export]
+// macro_rules! cmdtest {
+//     ($name:ident, $cmd:expr, $fun:expr) => {
+//         #[test]
+//         fn $name() {
+//             let command = Command::new($cmd);
+//             let mut cmd = TestCommand {
+//                 dir: "".to_string(),
+//                 cmd: command,
+//             };
+//             $fun(cmd);
+//         }
+//     };
+// }
 
-#[macro_export]
-macro_rules! eqnice {
-    ($expected:expr, $got:expr) => {
-        let expected = &*$expected;
-        let got = &*$got;
-        if expected != got {
-            panic!(
-                "
-printed outputs differ!
-expected:
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-{}
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-got:
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-{}
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-",
-                expected, got
-            );
-        }
-    };
-}
+// #[macro_export]
+// macro_rules! eqnice {
+//     ($expected:expr, $got:expr) => {
+//         let expected = &*$expected;
+//         let got = &*$got;
+//         if expected != got {
+//             panic!(
+//                 "
+// printed outputs differ!
+// expected:
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// {}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// got:
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// {}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ",
+//                 expected, got
+//             );
+//         }
+//     };
+// }
 
-#[macro_export]
-macro_rules! eqnice_repr {
-    ($expected:expr, $got:expr) => {
-        let expected = &*$expected;
-        let got = &*$got;
-        if expected != got {
-            panic!(
-                "
-printed outputs differ!
-expected:
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-{:?}
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-got:
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-{:?}
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-",
-                expected, got
-            );
-        }
-    };
-}
+// #[macro_export]
+// macro_rules! eqnice_repr {
+//     ($expected:expr, $got:expr) => {
+//         let expected = &*$expected;
+//         let got = &*$got;
+//         if expected != got {
+//             panic!(
+//                 "
+// printed outputs differ!
+// expected:
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// {:?}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// got:
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// {:?}
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ",
+//                 expected, got
+//             );
+//         }
+//     };
+// }
 
-/// A simple wrapper around a process::Command with some verification conveniences.
-/// this was heavily influenced by the excellent rust writer: BurntSushi
-/// https://github.com/BurntSushi/ripgrep/blob/7a6a40bae18f89bcfc6997479d7202d2c098e964/tests/util.rs
-#[derive(Debug)]
-pub struct TestCommand {
-    /// The dir used to launched this command.
-    dir: String,
-    /// The actual command we use to control the process.
-    cmd: Command,
-}
+// /// A simple wrapper around a process::Command with some verification conveniences.
+// /// this was heavily influenced by the excellent rust writer: BurntSushi
+// /// https://github.com/BurntSushi/ripgrep/blob/7a6a40bae18f89bcfc6997479d7202d2c098e964/tests/util.rs
+// #[derive(Debug)]
+// pub struct TestCommand {
+//     /// The dir used to launched this command.
+//     dir: String,
+//     /// The actual command we use to control the process.
+//     cmd: Command,
+// }
 
-impl TestCommand {
-    /// Returns a mutable reference to the underlying command.
-    pub fn cmd(&mut self) -> &mut Command {
-        &mut self.cmd
-    }
+// impl TestCommand {
+//     /// Returns a mutable reference to the underlying command.
+//     pub fn cmd(&mut self) -> &mut Command {
+//         &mut self.cmd
+//     }
 
-    /// Add an argument to pass to the command.
-    pub fn arg<A: AsRef<OsStr>>(&mut self, arg: A) -> &mut TestCommand {
-        self.cmd.arg(arg);
-        self
-    }
+//     /// Add an argument to pass to the command.
+//     pub fn arg<A: AsRef<OsStr>>(&mut self, arg: A) -> &mut TestCommand {
+//         self.cmd.arg(arg);
+//         self
+//     }
 
-    /// Add any number of arguments to the command.
-    pub fn args<I, A>(&mut self, args: I) -> &mut TestCommand
-    where
-        I: IntoIterator<Item = A>,
-        A: AsRef<OsStr>,
-    {
-        self.cmd.args(args);
-        self
-    }
+//     /// Add any number of arguments to the command.
+//     pub fn args<I, A>(&mut self, args: I) -> &mut TestCommand
+//     where
+//         I: IntoIterator<Item = A>,
+//         A: AsRef<OsStr>,
+//     {
+//         self.cmd.args(args);
+//         self
+//     }
 
-    /// Set the working directory for this command.
-    ///
-    /// Note that this does not need to be called normally, since the creation
-    /// of this TestCommand causes its working directory to be set to the
-    /// test's directory automatically.
-    pub fn current_dir<P: AsRef<Path>>(&mut self, dir: P) -> &mut TestCommand {
-        self.cmd.current_dir(dir);
-        self
-    }
+//     /// Set the working directory for this command.
+//     ///
+//     /// Note that this does not need to be called normally, since the creation
+//     /// of this TestCommand causes its working directory to be set to the
+//     /// test's directory automatically.
+//     pub fn current_dir<P: AsRef<Path>>(&mut self, dir: P) -> &mut TestCommand {
+//         self.cmd.current_dir(dir);
+//         self
+//     }
 
-    /// Runs and captures the stdout of the given command.
-    pub fn stdout(&mut self) -> String {
-        let o = self.output();
-        let stdout = String::from_utf8_lossy(&o.stdout);
-        match stdout.parse() {
-            Ok(t) => t,
-            Err(err) => {
-                panic!("could not convert from string: {:?}\n\n{}", err, stdout);
-            }
-        }
-    }
+//     /// Runs and captures the stdout of the given command.
+//     pub fn stdout(&mut self) -> String {
+//         let o = self.output();
+//         let stdout = String::from_utf8_lossy(&o.stdout);
+//         match stdout.parse() {
+//             Ok(t) => t,
+//             Err(err) => {
+//                 panic!("could not convert from string: {:?}\n\n{}", err, stdout);
+//             }
+//         }
+//     }
 
-    /// Gets the output of a command. If the command failed, then this panics.
-    pub fn output(&mut self) -> process::Output {
-        let output = self.cmd.output().unwrap();
-        self.expect_success(output)
-    }
+//     /// Gets the output of a command. If the command failed, then this panics.
+//     pub fn output(&mut self) -> process::Output {
+//         let output = self.cmd.output().unwrap();
+//         self.expect_success(output)
+//     }
 
-    /// Runs the command and asserts that it resulted in an error exit code.
-    pub fn assert_err(&mut self) {
-        let o = self.cmd.output().unwrap();
-        if o.status.success() {
-            panic!(
-                "\n\n===== {:?} =====\n\
-                 command succeeded but expected failure!\
-                 \n\ncwd: {}\
-                 \n\nstatus: {}\
-                 \n\nstdout: {}\n\nstderr: {}\
-                 \n\n=====\n",
-                self.cmd,
-                self.dir,
-                o.status,
-                String::from_utf8_lossy(&o.stdout),
-                String::from_utf8_lossy(&o.stderr)
-            );
-        }
-    }
+//     /// Runs the command and asserts that it resulted in an error exit code.
+//     pub fn assert_err(&mut self) {
+//         let o = self.cmd.output().unwrap();
+//         if o.status.success() {
+//             panic!(
+//                 "\n\n===== {:?} =====\n\
+//                  command succeeded but expected failure!\
+//                  \n\ncwd: {}\
+//                  \n\nstatus: {}\
+//                  \n\nstdout: {}\n\nstderr: {}\
+//                  \n\n=====\n",
+//                 self.cmd,
+//                 self.dir,
+//                 o.status,
+//                 String::from_utf8_lossy(&o.stdout),
+//                 String::from_utf8_lossy(&o.stderr)
+//             );
+//         }
+//     }
 
-    /// Runs the command and asserts that its exit code matches expected exit
-    /// code.
-    pub fn assert_exit_code(&mut self, expected_code: i32) {
-        let code = self.cmd.output().unwrap().status.code().unwrap();
-        assert_eq!(
-            expected_code, code,
-            "\n\n===== {:?} =====\n\
-             expected exit code did not match\
-             \n\nexpected: {}\
-             \n\nfound: {}\
-             \n\n=====\n",
-            self.cmd, expected_code, code
-        );
-    }
+//     /// Runs the command and asserts that its exit code matches expected exit
+//     /// code.
+//     pub fn assert_exit_code(&mut self, expected_code: i32) {
+//         let code = self.cmd.output().unwrap().status.code().unwrap();
+//         assert_eq!(
+//             expected_code, code,
+//             "\n\n===== {:?} =====\n\
+//              expected exit code did not match\
+//              \n\nexpected: {}\
+//              \n\nfound: {}\
+//              \n\n=====\n",
+//             self.cmd, expected_code, code
+//         );
+//     }
 
-    /// Runs the command and asserts that something was printed to stderr.
-    pub fn assert_non_empty_stderr(&mut self) {
-        let o = self.cmd.output().unwrap();
-        if o.status.success() || o.stderr.is_empty() {
-            panic!(
-                "\n\n===== {:?} =====\n\
-                 command succeeded but expected failure!\
-                 \n\ncwd: {}\
-                 \n\nstatus: {}\
-                 \n\nstdout: {}\n\nstderr: {}\
-                 \n\n=====\n",
-                self.cmd,
-                self.dir,
-                o.status,
-                String::from_utf8_lossy(&o.stdout),
-                String::from_utf8_lossy(&o.stderr)
-            );
-        }
-    }
+//     /// Runs the command and asserts that something was printed to stderr.
+//     pub fn assert_non_empty_stderr(&mut self) {
+//         let o = self.cmd.output().unwrap();
+//         if o.status.success() || o.stderr.is_empty() {
+//             panic!(
+//                 "\n\n===== {:?} =====\n\
+//                  command succeeded but expected failure!\
+//                  \n\ncwd: {}\
+//                  \n\nstatus: {}\
+//                  \n\nstdout: {}\n\nstderr: {}\
+//                  \n\n=====\n",
+//                 self.cmd,
+//                 self.dir,
+//                 o.status,
+//                 String::from_utf8_lossy(&o.stdout),
+//                 String::from_utf8_lossy(&o.stderr)
+//             );
+//         }
+//     }
 
-    fn expect_success(&self, o: process::Output) -> process::Output {
-        if !o.status.success() {
-            let suggest = if o.stderr.is_empty() {
-                "\n\nDid your search end up with no results?".to_string()
-            } else {
-                "".to_string()
-            };
+//     fn expect_success(&self, o: process::Output) -> process::Output {
+//         if !o.status.success() {
+//             let suggest = if o.stderr.is_empty() {
+//                 "\n\nDid your search end up with no results?".to_string()
+//             } else {
+//                 "".to_string()
+//             };
 
-            panic!(
-                "\n\n==========\n\
-                 command failed but expected success!\
-                 {}\
-                 \n\ncommand: {:?}\
-                 \ncwd: {}\
-                 \n\nstatus: {}\
-                 \n\nstdout: {}\
-                 \n\nstderr: {}\
-                 \n\n==========\n",
-                suggest,
-                self.cmd,
-                self.dir,
-                o.status,
-                String::from_utf8_lossy(&o.stdout),
-                String::from_utf8_lossy(&o.stderr)
-            );
-        }
-        o
-    }
-}
+//             panic!(
+//                 "\n\n==========\n\
+//                  command failed but expected success!\
+//                  {}\
+//                  \n\ncommand: {:?}\
+//                  \ncwd: {}\
+//                  \n\nstatus: {}\
+//                  \n\nstdout: {}\
+//                  \n\nstderr: {}\
+//                  \n\n==========\n",
+//                 suggest,
+//                 self.cmd,
+//                 self.dir,
+//                 o.status,
+//                 String::from_utf8_lossy(&o.stdout),
+//                 String::from_utf8_lossy(&o.stderr)
+//             );
+//         }
+//         o
+//     }
+// }
 
-cmdtest!(test_ls, "ls", |mut cmd: TestCommand| {
-    cmd.current_dir("..");
-    let expected = "\
-1:Watson
-1:Sherlock
-2:Holmes
-3:Sherlock Holmes
-5:Watson
-";
-    eqnice!(expected, cmd.stdout());
-});
+// cmdtest!(test_ls, "ls", |mut cmd: TestCommand| {
+//     cmd.current_dir("..");
+//     let expected = "\
+// 1:Watson
+// 1:Sherlock
+// 2:Holmes
+// 3:Sherlock Holmes
+// 5:Watson
+// ";
+//     eqnice!(expected, cmd.stdout());
+// });
 
-fn tester() {
-    fn f() {
-        assert!{false};
-    }
-    let os = TestOpts {
-        filter: None,
-        run_ignored: false,
-        run_tests: true,
-        bench_benchmarks: false,
-        logfile: None,
-        nocapture: true,
-        color: AutoColor,
-        verbose: true,
-    };
-    let desc = TestDescAndFn {
-        desc: TestDesc {
-            name: StaticTestName("whatever"),
-            ignore: false,
-            should_panic: ShouldPanic::No,
-            allow_fail: false,
-        },
-        testfn: DynTestFn(Box::new(move || f())),
-    };
-    let (tx, rx) = channel();
-    run_test(&os, false, desc, tx);
-    let (a, res, c) = rx.recv().unwrap();
+// fn tester() {
+//     fn f() {
+//         println!("tesdasdadasdt");
+//         assert!{false};
+//     }
+//     let os = TestOpts {
+//         filter: None,
+//         run_ignored: false,
+//         run_tests: true,
+//         bench_benchmarks: false,
+//         logfile: None,
+//         nocapture: false,
+//         color: AutoColor,
+//         verbose: true,
+//     };
+//     let desc = TestDescAndFn {
+//         desc: TestDesc {
+//             name: StaticTestName("this is a test"),
+//             ignore: false,
+//             should_panic: ShouldPanic::No,
+//             allow_fail: false,
+//         },
+//         testfn: DynTestFn(Box::new(move || f())),
+//     };
+//     let (tx, rx) = channel();
+//     run_test(&os, false, desc, tx);
+//     let (a, res, c) = rx.recv().unwrap();
      
-    println!("{:?} {:?}", a, c)
-    //assert!(res == TrIgnored);
-}
-fn main(){
-    tester();
-    tester();
+//     println!("{:?} {:?}", a, c)
+//     //assert!(res == TrIgnored);
+// }
+// fn main(){
+//     tester();
 
+//     tester();
+
+// }
+
+fn main() {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let destination = std::path::Path::new(&out_dir).join("gen_tests.rs");
+    let mut f = std::fs::File::create(&destination).unwrap();
+
+    let params = &["abc", "fooboo"];
+    for p in params {
+        use std::io::Write;
+        write!(
+            f,
+            "
+#[test]
+fn {name}() {{
+    assert!(true);
+}}",
+            name = p
+        ).unwrap();
+    }
 }
