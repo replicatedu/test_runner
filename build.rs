ca12abd20a1ad843a310a507afa33b40ee19711e
alex <hortinstein@gmail.com>
2019-03-04 21:09:57 -0500

    build.c

--- build.rs
+++ build.rs
@@ -1,19 +1,19 @@
 fn main() {
-//     let out_dir = std::env::var("OUT_DIR").unwrap();
-//     let destination = std::path::Path::new(&out_dir).join("gen_tests.rs");
-//     let mut f = std::fs::File::create(&destination).unwrap();
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let destination = std::path::Path::new(&out_dir).join("gen_tests.rs");
+    let mut f = std::fs::File::create(&destination).unwrap();
 
-//     let params = &["abc", "fooboo"];
-//     for p in params {
-//         use std::io::Write;
-//         write!(
-//             f,
-//             "
-// #[test]
-// fn {name}() {{
-//     assert!(true);
-// }}",
-//             name = p
-//         ).unwrap();
-//     }
+    let params = &["abc", "fooboo","asdasd"];
+    for p in params {
+        use std::io::Write;
+        write!(
+            f,
+            "
+#[test]
+fn {name}() {{
+    assert!(true);
+}}",
+            name = p
+        ).unwrap();
+    }
 }
