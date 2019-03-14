8d996fb5affe8a40870431b0bb43c3e8eaddeaf2
alex <hortinstein@gmail.com>
2019-03-13 22:46:58 -0400

    got tests kind of working now

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 extern crate difference;
 extern crate term;
 
+use std::fs;
+
 use difference::{Changeset, Difference};
 use std::ffi::OsStr;
 use std::path::Path;
@@ -10,9 +12,9 @@ use std::process::{self, Command};
 pub fn percentage_diff(got: &str, expected: &str) -> (f64, String) {
     let mut t = term::stdout().unwrap();
     let Changeset { diffs, .. } = Changeset::new(got.trim(), expected.trim(), "");
-    let mut len_same:usize = 0;
-    let mut len_rem:usize = 0;
-    let mut outdiff:String = String::new();
+    let mut len_same: usize = 0;
+    let mut len_rem: usize = 0;
+    let mut outdiff: String = String::new();
     for i in 0..diffs.len() {
         match diffs[i] {
             Difference::Same(ref x) => {
@@ -27,10 +29,10 @@ pub fn percentage_diff(got: &str, expected: &str) -> (f64, String) {
                 outdiff.push_str(&format!("-{}", x));
             }
         }
-    } 
+    }
     t.fg(term::color::CYAN).unwrap();
-    let total = len_same+len_rem;
-    let percentage:f64 = len_same as f64 / total as f64 ;
+    let total = len_same + len_rem;
+    let percentage: f64 = len_same as f64 / total as f64;
 
     //writeln!(t, "%{} ({}/{})", percentage*100.0,len_same,total).expect("failed to print line");
     t.reset().unwrap();
@@ -106,7 +108,7 @@ impl TestCommand {
         if o.status.success() {
             panic!(
                 "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\
-                \n===== {:?} =====\n\
+                 \n===== {:?} =====\n\
                  command succeeded but expected failure!\
                  \ncwd: {}\
                  \nstatus: {}\
@@ -127,15 +129,18 @@ impl TestCommand {
         let o = self.cmd.output().unwrap();
         let code = o.status.code().unwrap();
         assert_eq!(
-            expected_code, code,
+            expected_code,
+            code,
             "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\
-            \n===== {:?} =====\n\
+             \n===== {:?} =====\n\
              expected exit code did not match\
              \nexpected: {}\
              \nfound: {}\
              \nstdout: {}\n\nstderr: {}\
              \n=====\n",
-            self.cmd, expected_code, code,
+            self.cmd,
+            expected_code,
+            code,
             String::from_utf8_lossy(&o.stdout),
             String::from_utf8_lossy(&o.stderr)
         );
@@ -147,7 +152,7 @@ impl TestCommand {
         if o.status.success() || o.stderr.is_empty() {
             panic!(
                 "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\
-                \n===== {:?} =====\n\
+                 \n===== {:?} =====\n\
                  command succeeded but expected failure!\
                  \ncwd: {}\
                  \nstatus: {}\
@@ -162,14 +167,14 @@ impl TestCommand {
         }
     }
 
-    pub fn expect_output_threshold(&mut self, target_threshold: f64, expected:&str) {
+    pub fn expect_output_threshold(&mut self, target_threshold: f64, expected: &str) {
         let o = self.cmd.output().unwrap();
         let stdout = String::from_utf8_lossy(&o.stdout);
-        let (perc_diff, out) = percentage_diff(&stdout,&expected);
+        let (perc_diff, out) = percentage_diff(&stdout, &expected);
         if target_threshold > perc_diff {
-             panic!(
+            panic!(
                 "\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\
-                \n===== {:?} =====\n\
+                 \n===== {:?} =====\n\
                  only {} matched expected output, required {}!\
                  \ncwd: {}\
                  \nstatus: {}\
@@ -186,7 +191,6 @@ impl TestCommand {
                 out
             );
         }
-
     }
 
     fn expect_success(&self, o: process::Output) -> process::Output {
@@ -216,5 +220,141 @@ impl TestCommand {
             );
         }
         o
+    }
+}
+
+extern crate toml;
+#[macro_use]
+extern crate serde_derive;
+
+//returns a command setup ready to run the tests
+fn setup_command(test_command: &str, test_directory: String) -> TestCommand {
+    let command = if cfg!(target_os = "windows") {
+        {
+            let mut c = Command::new("cmd");
+            c.args(&["/C", test_command]);
+            c
+        }
+    } else {
+        {
+            let mut c = Command::new("sh");
+            c.arg("-c");
+            c.arg("test_command");
+            c
+        }
+    };
+    let mut cmd = TestCommand {
+        dir: ".".to_string(),
+        cmd: command,
+    };
+    cmd.current_dir(test_directory);
+    cmd
+}
+
+//tests to ensure an error propigates
+fn test_assert_err(mut tester: TestCommand) {
+    tester.assert_err();
+}
+
+//tests to see if there is an exit code
+fn test_assert_exit_code(mut tester: TestCommand, test_config: &Test) {
+    tester.assert_exit_code(
+        test_config
+            .exit_code()
+            .expect("test did not have a proper exit code"),
+    );
+}
+
+//test to see if there is a stderr
+fn test_assert_non_empty_stderr(mut tester: TestCommand) {
+    tester.assert_non_empty_stderr();
+}
+
+//checks to see if output threshold is made
+fn test_expect_output_threshold(mut tester: TestCommand, test_config: &Test) {
+    tester.expect_output_threshold(
+        test_config
+            .target_threshold()
+            .expect("test did not have a threshold"),
+        &test_config
+            .expected()
+            .expect("test did not have a expected var"),
+    );
+}
+
+//checks to see if output identical
+fn test_output_is_expected(mut tester: TestCommand, test_config: &Test) {
+    tester.expect_output_threshold(
+        1.0,
+        &test_config
+            .expected()
+            .expect("test did not have a expected var"),
+    );
+}
+
+pub fn broker_test(test: &Test) {
+    let s = test.test_type().unwrap();
+    let s_slice: &str = &s[..];
+    let mut test_command = setup_command(&test.cmd().unwrap(), test.test_directory().unwrap());
+    match s_slice {
+        "test_assert_err" => test_assert_err(test_command),
+        "test_assert_exit_code" => test_assert_exit_code(test_command, test),
+        "test_assert_non_empty_stderr" => test_assert_exit_code(test_command,test),
+        "test_expect_output_threshold" => test_expect_output_threshold(test_command, test),
+        "test_output_is_expected" => test_output_is_expected(test_command, test),
+        _ => panic!("test case unsupported")
+    }
+}
+
+
+/// This is what we're going to decode into. Each field is optional, meaning
+/// that it doesn't have to be present in TOML.
+#[derive(Debug, Deserialize)]
+pub struct TestDoc {
+    pub test_file_description: Option<String>,
+    pub test: Option<Vec<Test>>,
+}
+
+/// Sub-structs are decoded from tables, so this will decode from the `[server]`
+/// table.
+///
+/// Again, each field is optional, meaning they don't have to be present.
+#[derive(Debug, Deserialize)]
+pub struct Test {
+    description: Option<String>,
+    test_type: Option<String>,
+    test_directory: Option<String>,
+    exit_code: Option<i32>,
+    target_threshold: Option<f64>,
+    cmd: Option<String>,
+    expected: Option<String>,
+    points: Option<u64>,
+}
+
+impl Test {
+    // Immutable access.
+    pub fn description(&self) -> Option<String> {
+        self.description.clone()
+    }
+    pub fn test_type(&self) -> Option<String> {
+        self.test_type.clone()
+    }
+    pub fn test_directory(&self) -> Option<String> {
+        self.test_directory.clone()
+    }
+    pub fn exit_code(&self) -> Option<i32> {
+        self.exit_code.clone()
+    }
+    pub fn target_threshold(&self) -> Option<f64> {
+        self.target_threshold.clone()
+    }
+    pub fn cmd(&self) -> Option<String> {
+        self.cmd.clone()
+    }
+    pub fn expected(&self) -> Option<String> {
+        self.test_type.clone()
+    }
+    pub fn points(&self) -> Option<u64> {
+        self.points.clone()
     }
 }
